73620d5acb7a0a147b1539e040b82c10c5a385da
Arie1150 <arierezky17@gmail.com>
2026-02-02 00:55:33 +0800

    Now

--- routes/web.php
+++
@@ -1,6 +0,0 @@
-<?php
-use App\Http\Controllers\TodoController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/todos', [TodoController::class, 'index']);
-Route::post('/todos', [TodoController::class, 'store']);
